ab05ce916858b152dac9ad6d7ddd4a6b603562c3
davidrguez98 <ropeda98@gmail.com>
2025-06-05 18:43:39 +0200

    funcion crear sin mensaje

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
+function clipBoardCopy(text) {
+    navigator.clipboard.writeText(text)
+}
+
 function main() {
+
+    document.querySelectorAll("#copyEmailBtn").forEach(btn => {
+        btn.addEventListener("click", function() {
+            clipBoardCopy('ropeda98@gmail.com')
+        })
+    })
+    
 
     window.addEventListener("scroll", function () {
         const navbar = document.querySelector(".navbar");
@@ -8,6 +19,7 @@ function main() {
             navbar.classList.remove("scrolled");
         }
     })
+
 
 }
 
