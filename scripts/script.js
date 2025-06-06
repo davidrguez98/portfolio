be03c80c79399492564239fc663a32b4c8a4175b
davidrguez98 <ropeda98@gmail.com>
2025-06-06 13:06:13 +0200

    menu hamburguesa

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,26 @@ function main() {
             clipBoardCopy('ropeda98@gmail.com')
         })
     })
-    
+
+    document.querySelector(".burgerMenu").addEventListener("click", () => {
+        document.querySelector("#nav").classList.add("visible")
+    })
+
+    document.querySelector("#closeButton").addEventListener("click", () => {
+        document.querySelector("#nav").classList.remove("visible")
+        console.log("kahjsd")
+
+    })
+
+    document.querySelectorAll(".nav-link").forEach(btn => {
+        btn.addEventListener("click", () => {
+            document.querySelector("#nav").classList.remove("visible")
+        })
+
+    })
 
     window.addEventListener("scroll", function () {
-        const navbar = document.querySelector(".navbar");
+        const navbar = document.querySelector("nav");
         if (window.scrollY > 10) {
             navbar.classList.add("scrolled");
         } else {
